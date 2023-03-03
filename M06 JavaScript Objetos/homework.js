b293b67cde99376a65747c4243d959f2c13549c5
leandrofiore3 <leandrofiore3@gmail.com>
2023-03-03 15:57:05 -0300

    finalizado el 05

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -41,6 +41,7 @@ function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   
 }
 
 function tieneEmail(objetoUsuario) {
