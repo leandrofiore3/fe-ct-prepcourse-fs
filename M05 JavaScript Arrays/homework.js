a7c37418ee07b58b9b6ebc54b554819e5cdd692f
leandrofiore3 <leandrofiore3@gmail.com>
2023-03-03 15:58:08 -0300

    finalizado el 05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -213,6 +213,21 @@ function breakStatement(num) {
    // la ejecución y retornar el string: "Se interrumpió la ejecución".
    // [PISTA]: utiliza el statement 'break'.
    // Tu código:
+   var array = [];
+   var suma = num;
+   for(var i= 0; i<10; i++) {
+    suma = suma + 2;
+    if(suma === i) break;
+    else {
+       array.push(suma);
+    }
+   }
+   if(i < 10) {
+     return 'Se interrumpió la ejecución';
+   }
+   else {
+       return array;
+   }
 }
 
 function continueStatement(num) {
@@ -222,6 +237,16 @@ function continueStatement(num) {
    // se continua con la siguiente iteración.
    // [PISTA]: utiliza el statement 'continue'.
    // Tu código:
+   var array = [];
+   var suma = num;
+   for(var i= 0; i<10; i++) {
+     if(i === 5) continue;
+     else {
+       suma = suma + 2;
+       array.push(suma);
+     }
+   }
+   return array;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
